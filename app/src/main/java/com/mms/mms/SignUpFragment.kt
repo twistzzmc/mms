d44626422aae19cfee762b5fd650696c0aa28e6a
twistzzmc <michal.szczepaniak1999@gmail.com>
2020-08-26 16:50:15 +0200

    changes

--- app/src/main/java/com/mms/mms/SignUpFragment.kt
+++ app/src/main/java/com/mms/mms/SignUpFragment.kt
@@ -1,4 +1,28 @@
 package com.mms.mms
 
-class SignUpFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+
+class SignUpFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.button_login).setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+    }
 }
