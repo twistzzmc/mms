eadb83689b1d6240cf041fde9b8c94dae51114bd
twistzzmc <michal.szczepaniak1999@gmail.com>
2020-08-26 17:42:41 +0200

    sign up

---
+++ app/src/main/java/com/mms/mms/SignUpActivity.kt
@@ -0,0 +1,19 @@
+package com.mms.mms
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+}
