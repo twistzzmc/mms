55f0adc8166e79aa5c0326324dd41dbe65bbfeba
twistzzmc <michal.szczepaniak1999@gmail.com>
2020-08-28 17:41:31 +0200

    login change

--- app/src/main/java/com/mms/mms/FirstFragment.kt
+++ app/src/main/java/com/mms/mms/FirstFragment.kt
@@ -7,6 +7,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
@@ -29,13 +31,28 @@ class FirstFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         view.findViewById<Button>(R.id.button_login).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+            val userName = view.findViewById<EditText>(R.id.et_user_name).text.toString()
+            val password = view.findViewById<EditText>(R.id.et_password).text.toString()
+
+            var validInput = true
+            //TODO check if input is valid and present in database
+
+            if (validInput) {
+                findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+            }
+            else {
+                Toast.makeText(this@FirstFragment.context, "Incorrect user name or password!", Toast.LENGTH_SHORT).show()
+            }
         }
 
         view.findViewById<Button>(R.id.button_sign_up).setOnClickListener {
             val intent = Intent(this@FirstFragment.context, SignUpActivity::class.java)
             startActivity(intent)
 //            findNavController().navigate(R.id.action_FirstFragment_to_SignUpFragment)
+        }
+
+        view.findViewById<TextView>(R.id.reset_password).setOnClickListener {
+            Toast.makeText(this@FirstFragment.context, "Reset password TODO", Toast.LENGTH_SHORT).show()
         }
     }
 }
